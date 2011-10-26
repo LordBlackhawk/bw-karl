cbdf973357ad0f586bac5d3f29f20ef7498585ae
LordBlackhawk <lordblackhawk@gmail.com>
2011-10-26 10:02:57 +0000

    'resources.h': Introduced LOCKLIST. inc/dec/set/get[Locked] introduced. setTime/getTime/advance added.

--- planing/resources.h
+++ planing/resources.h
@@ -7,19 +7,20 @@ template <class RLIST>
 class Resources
 {
   typedef Resources<RLIST> ThisType;
+  typedef sublist< Plan::ResourceLockable, RLIST > LOCKLIST;
   
   public:
-     Resources() : amount(0), locked(0)
+     Resources() : time(0), amount(0), locked(0)
      { }
      
-     Resources(const Resources& r) : amount(r.amount), locked(r.locked)
+     Resources(const Resources& r) : time(r.time), amount(r.amount), locked(r.locked)
      { }
 	 
-	 void swap(ThisType& other)
-	 {
-		amount.swap(other.amount);
-		locked.swap(other.locked);
-	 }
+	   void swap(ThisType& other)
+	   {
+        amount.swap(other.amount);
+        locked.swap(other.locked);
+     }
 	
      bool valid() const
      {
@@ -31,26 +32,14 @@ class Resources
             return false;
         return true;
      }
-     
-     template <class RT>
-     int& get()
-     {
-       return amount.get<RT>();
-     }
 	 
-	 template <class RT>
+	   template <class RT>
      int get() const
      {
        return amount.get<RT>();
      }
      
-     template <class RT>
-     int& getLocked()
-     {
-       return locked.get<RT>();
-     }
-	 
-	 template <class RT>
+	   template <class RT>
      int getLocked() const
      {
        return locked.get<RT>();
@@ -61,37 +50,63 @@ class Resources
      {
        return amount.get<RT>() + locked.get<RT>();
      }
+     
+     template <class RT>
+     void set(int value)
+     {
+       amount.get<RT>() = value;
+     }
+     
+     template <class RT>
+     void setLocked(int value)
+     {
+       locked.get<RT>() = value;
+     }
+     
+     template <class RT>
+     void inc(int optime, int value = 1)
+     {
+       amount.get<RT>() += value;
+       // TODO: ResGrowth
+     }
+     
+     template <class RT>
+     void dec(int optime, int value = 1)
+     {
+       inc(optime, -value);
+     }
 	 
-	 template <class RT>
-	 void incLocked(int value = 1)
-	 {
-		amount.get<RT>() -= value;
-		locked.get<RT>() += value;
-	 }
+	   template <class RT>
+     void incLocked(int optime, int value = 1)
+	   {
+        dec(optime, value);
+        locked.get<RT>() += value;
+     }
 	 
-	 template <class RT>
-	 void decLocked(int value = 1)
-	 {
-		amount.get<RT>() += value;
-		locked.get<RT>() -= value;
-	 }
-     
-     int& get(const ResourceIndex<RLIST>& ri)
+     template <class RT>
+     void decLocked(int optime, int value = 1)
      {
-       return amount[ri.getIndex()];
+        incLocked(optime, -value);
      }
      
-     int& getLocked(const ResourceIndex<RLIST>& ri)
+     void advance(int dt)
      {
-       return locked[ri.getIndex()];
+       // TODO: ResGrowth
+       time += dt;
      }
      
-     int getExisting(const ResourceIndex<RLIST>& ri) const
+     void setTime(int newtime)
      {
-       return amount[ri.getIndex()] + locked[ri.getIndex()];
+       time = newtime;
      }
      
+     int getTime() const
+     {
+       return time;
+     }
+
   protected:
-    Tuple<RLIST, int> amount;
-    Tuple<RLIST, int> locked;
+    int                  time;
+    Tuple<RLIST, int>    amount;
+    Tuple<LOCKLIST, int> locked;
 };
