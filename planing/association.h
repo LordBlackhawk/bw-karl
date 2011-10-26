620a331152d97f23844b1c0543442ac988297459
LordBlackhawk <lordblackhawk@gmail.com>
2011-10-26 13:34:58 +0000

    DEF_SIMPLEREQUIREMENT added to 'association.h'.

--- planing/association.h
+++ planing/association.h
@@ -8,14 +8,34 @@ namespace Plan {
 	{
 		static T value;
 	};
+   
+  template <class RT, class OT>
+  struct SimpleRequirement
+  {
+    enum { value = false };
+  };
 }
 
-#define DEF_ASSOCIATION(Name, Type, Value)																\
-	namespace Operations { struct Name; }																\
-	template <> static Type Plan::OperationAssociations<Operations::Name, Type>::value = Value;
+#define DEF_ASSOCIATION(Name, Type, Value)		 \
+	struct Name;																\
+	template <> static Type Plan::OperationAssociations<Name, Type>::value = Value;
 
 template <class OT, class T>
 T getOperationAssociation()
 {
 	return Plan::OperationAssociation<OT, T>::value;
 }
+
+#define DEF_SIMPLEREQUIREMENT(RName, OName)                            \
+  namespace Plan { template <> struct SimpleRequirement<RName, OName>  \
+  { enum { value = true }; }; }
+
+template <class OLIST, class RT>
+struct SimpleRequirementList
+{
+  template <class OT>
+  struct Predicate
+  { enum { value = Plan::SimpleRequirement<RT, OT>::value }; };
+  
+  typedef sublist< Predicate, OLIST >::type type;
+};
