59cdf865c08cd2070b439b0d5f85430f7aaa54d1
LordBlackhawk <lordblackhawk@gmail.com>
2011-10-27 12:46:33 +0000

    'displayres.h' added.

---
+++ bwplan/displayres.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "bwplan.h"
+#include <BWAPI.h>
+
+void displayResources(const BWResources& res)
+{
+	Broodwar->drawTextScreen(5, 0, "At game time %d, we have the following resources:", res.getTime());
+	int line = 1;
+	for (auto it : BWResourceIndices)
+		if (res.get(it) != 0)
+	{
+		if (it.isLockable()) {
+			Broodwar->drawTextScreen(5, 16*line, "- %d/%d %ss", res.get(it), res.getLocked(it), it.getName());
+		} else {
+			Broodwar->drawTextScreen(5, 16*line, "-   %d  %ss", res.get(it), it.getName());
+		}
+		++line;
+	}
+}
