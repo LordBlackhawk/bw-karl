b73de477706f2ecec5eb2661445d3ee8a583b1f9
LordBlackhawk <lordblackhawk@gmail.com>
2011-11-08 12:39:47 +0000

    PlanDesigner uses parameter-reader.h

--- bwplandesigner/PlanDesigner.cpp
+++ bwplandesigner/PlanDesigner.cpp
@@ -1,51 +1,23 @@
 #define  NO_ASSOCIATIONS
 #include "bwplan/bwplan.h"
 #include "bwplan/stream-output.h"
+#include "bwplan/parameter-reader.h
 
 #include <iostream>
 
 int main(int argc, const char* argv[])
 {
-	if (argc < 2) {
-		std::cerr << "Use: PlanDesigner [Terran|Protoss|Zerg] [Operations...]\n";
+  BWParameterReader reader;
+  std::string savefilename;
+  reader.general.add_options()  ("save,s", po::value<std::string>(&savefilename), "Save build to file name.");
+  reader.run(argc, argv);
+	if (reader.showhelp) {
+		std::cerr << "Use: PlanDesigner [Options...] [Operations...]\n" << reader;
 		return 1;
 	}
-
-	std::string race = argv[1];
-	BWResources res;
-	res.set<RMinerals>(50);
-	if (race == "Terran") {
-		res.set<RTerranWorker>(4);
-		res.set<RTerranCommandCenter>(1);
-		res.set<RTerranSupply>(2*10);
-		res.incLocked<RTerranSupply>(0, 2*4);
-	} else if (race == "Protoss") {
-		res.set<RProtossWorker>(4);
-		res.set<RProtossNexus>(1);
-		res.set<RProtossSupply>(2*9);
-		res.incLocked<RProtossSupply>(0, 2*4);
-	} else if (race == "Zerg") {
-		res.set<RZergWorker>(4);
-		res.set<RZergHatchery>(1);
-		res.set<RZergSupply>(2*9);
-		res.incLocked<RZergSupply>(0, 2*4);
-		res.set<RLarva>(3);
-	} else {
-		std::cerr << "Unknown Race: " << race << "\n";
-		return 1;
-	}
-
-	BWPlan plan(res, 0);
-	std::cout << "Parsing arguments...\n";
-	for (int k=2; k<argc; ++k) {
-		BWOperationIndex index = BWOperationIndex::byUserName(argv[k]);
-		if (!index.valid()) {
-			std::cerr << "Unknown Operation Name: " << argv[k] << "\n";
-			continue;
-		}
-		if (!plan.push_back_sr(BWOperation(index)))
-			std::cerr << "Unable to add " << index.getName() << "!\n";
-	}
+  
+  std::cout << "Parsing arguments...\n";
+  BWPlan plan = reader.getStartPlan();
 	std::cout << "\n";
 
 	std::cout << "Planed Operations:\n";
@@ -61,7 +33,8 @@ int main(int argc, const char* argv[])
 
 	std::cout << "Plan finished after " << outTime(plan.end().time()) << " frames.\n";
 	
-	plan.saveToFile("default.bwb");
+  if (savefilename != "")
+     plan.saveToFile(savefilename);
 	
 	return 0;
 }
