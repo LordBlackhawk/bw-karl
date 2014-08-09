6e9d9fae6162c702066841f40d88030d1d0839e6
LordBlackhawk <LordBlackhawk@gmail.com>
2014-08-09 16:34:46 +0200

    Added LarvaExpert.

---
+++ sources/expert/larva-expert.cpp
@@ -0,0 +1,34 @@
+#include "larva-expert.hpp"
+#include "expert-registrar.hpp"
+#include "plan/broodwar-ports.hpp"
+#include <algorithm>
+
+REGISTER_EXPERT(LarvaExpert)
+
+bool LarvaExpert::isApplicable(Blackboard* /*blackboard*/)
+{
+    return BWAPI::Broodwar->self()->getRace() == BWAPI::Races::Zerg;
+}
+
+void LarvaExpert::visitProvideUnitPort(ProvideUnitPort* port)
+{
+    if (port->getUnitType() != BWAPI::UnitTypes::Zerg_Larva)
+        return;
+    provide.push_back(port);
+}
+
+void LarvaExpert::visitRequireUnitPort(RequireUnitPort* port)
+{
+    if (port->getUnitType() != BWAPI::UnitTypes::Zerg_Larva)
+        return;
+    require.push_back(port);
+}
+
+void LarvaExpert::endTraversal()
+{
+    int size = std::min(provide.size(), require.size());
+    for (int k=0; k<size; ++k)
+        require[k]->connectTo(provide[k]);
+    provide.clear();
+    require.clear();
+}
---
+++ sources/tests/larva-expert-test.cpp
@@ -0,0 +1,29 @@
+#include "blackboard-fixture.hpp"
+#include "plan/broodwar-boundary-items.hpp"
+#include "plan/broodwar-plan-items.hpp"
+#include "expert/larva-expert.hpp"
+
+BOOST_FIXTURE_TEST_SUITE( larva_test, BlackboardFixture )
+
+BOOST_AUTO_TEST_CASE( basic )
+{
+    auto b1 = createOwnUnitBoundaryItem(BWAPI::UnitTypes::Zerg_Larva);
+    createOwnUnitBoundaryItem(BWAPI::UnitTypes::Zerg_Larva);
+    createOwnUnitBoundaryItem(BWAPI::UnitTypes::Zerg_Larva);
+
+    auto p1 = morphUnit(BWAPI::UnitTypes::Zerg_Drone);
+    auto p2 = morphUnit(BWAPI::UnitTypes::Zerg_Drone);
+
+    BOOST_CHECK_EQUAL( b1->provideUnit.getUnitType(), BWAPI::UnitTypes::Zerg_Larva );
+    BOOST_CHECK_EQUAL( p1->requireUnit.getUnitType(), BWAPI::UnitTypes::Zerg_Larva );
+    BOOST_CHECK_EQUAL( p1->ports.size(), 3U );
+    BOOST_CHECK( !p1->isActive() );
+
+    LarvaExpert expert;
+    expert.tick(blackboard);
+
+    BOOST_CHECK( p1->requireUnit.isConnected() );
+    BOOST_CHECK( p2->requireUnit.isConnected() );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
